fa6e0320f666b99ab32c31226c3a2c4f5c4e818e
angelozerr <angelo.zerr@gmail.com>
2018-03-16 08:45:55 +0100

    Consume freemarker-languageserver-all.jar

--- org.eclipse.lsp4e.freemarker/src/org/eclipse/lsp4e/freemarker/FreemarkerLanguageServer.java
+++ org.eclipse.lsp4e.freemarker/src/org/eclipse/lsp4e/freemarker/FreemarkerLanguageServer.java
@@ -1,8 +1,10 @@
 package org.eclipse.lsp4e.freemarker;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
 import org.osgi.framework.Bundle;
@@ -13,39 +15,19 @@ public class FreemarkerLanguageServer extends ProcessStreamConnectionProvider {
 		List<String> commands = new ArrayList<>();
 		commands.add("java");
 		commands.add("-jar");
-		commands.add("D:\\_Personal\\Freemarker\\org.lsp4fm\\target\\freemarker-server-all.jar");
-//		commands.add("-Declipse.application=org.eclipse.jdt.ls.core.id1");
-//		commands.add("-Dosgi.bundles.defaultStartLevel=4");
-//		commands.add("-Declipse.product=org.eclipse.jdt.ls.core.product");
-//		commands.add("-Dlog.protocol=true");
-//		commands.add("-Dlog.level=ALL");
-//		commands.add("-noverify");
-//		commands.add("-Xmx1G");
-//		commands.add("-jar");
-//		commands.add("./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar");
-//		commands.add("-configuration");
-//		if (Platform.getOS().equals(Platform.OS_WIN32)) {
-//			commands.add("./config_win");
-//		}
-//		if (Platform.getOS().equals(Platform.OS_LINUX)) {
-//			commands.add("./config_linux");
-//		}
-//		if (Platform.getOS().equals(Platform.OS_MACOSX)) {
-//			commands.add("./config_mac");
-//		}
-//		commands.add("-data");
-//		commands.add("./data");
+		commands.add("freemarker-languageserver-all.jar");
 
 		setCommands(commands);
 
 		Bundle bundle = Activator.getDefault().getBundle();
 		Path workingDir = Path.EMPTY;
-		//try {
-			workingDir = new Path("D:\\_Personal\\Freemarker\\org.lsp4fm\\target"); //new Path(FileLocator.toFileURL(FileLocator.find(bundle, new Path("server"), null)).getPath());
+		try {
+			workingDir = new Path(FileLocator.toFileURL(FileLocator.find(bundle, new Path("server"), null)).getPath());
 			setWorkingDirectory(workingDir.toOSString());
-//		} catch (IOException e) {
-//			LanguageServerPlugin.logError(e);
-//		}
+		} catch (IOException e) {
+			// LanguageServerPlugin.logError(e);
+			e.printStackTrace();
+		}
 	}
 
 	@Override
